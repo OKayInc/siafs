474a75c2dd5b3de0d0c5d35c0fb893eae6ed61f1
Luis Daniel Lucio Quiroz <dlucio@okay.com.mx>
2024-04-18 09:21:06 -0400

    fix CMake error

---
+++ sia_stats.c
@@ -0,0 +1,11 @@
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "sia_stats.h"
+extern sia_cfg_t opt;
+
+#ifdef __cplusplus
+}
+#endif
---
+++ sia_stats.h
@@ -0,0 +1,13 @@
+#ifndef SIA_STATS_H
+#define SIA_STATS_H
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "sia_common.h"
+
+#ifdef __cplusplus
+}
+#endif
+#endif
